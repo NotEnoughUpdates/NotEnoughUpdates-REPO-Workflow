0983877f59636846fc481e8be5d6aea3a04b5e8e
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-08-30 22:38:45 +0200

    oop

--- index.js
+++ index.js
@@ -172,11 +172,11 @@ async function run() {
                 }
             }
             if(!same){
-                core.warning('The lore of the nbt tag and the lore in the array is not the same, please fix this for item' + item + 'at line ' + 
+                core.warning('The lore of the nbt tag and the lore in the array is not the same, please fix this for item ' + item + ' at line ' + 
                 line + '\nNBT: ' + lines[line] + '\nLore: ' + file.lore[line])
                 annotations2.push({
                     title: 'The lore in the nbt tag and lore of ' + item + ' is not the same.',
-                    message: 'The lore of the nbt tag and the lore in the array is not the same, please fix this for item' + item  + 'at line ' + 
+                    message: 'The lore of the nbt tag and the lore in the array is not the same, please fix this for item ' + item  + ' at line ' + 
                     line + '\nNBT: ' + lines[line] + '\nLore: ' + file.lore[line],
                     annotation_level: 'warning',
                     path: item,
